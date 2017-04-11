1b01e478a576b347968a653e91e0ce82eb5279b5
Hans <hgallego@alumnos.ubiobio.cl>
2017-04-11 20:36:24 -0300

    Add the project root structure

---
+++ src/main/java/com/hm/eventos/EventosSocialesApiApplication.java
@@ -0,0 +1,12 @@
+package com.hm.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosSocialesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosSocialesApiApplication.class, args);
+	}
+}
